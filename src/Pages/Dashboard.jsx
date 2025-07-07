08c3b475062f1fe6a0201a08be7588af0697f22f
shivam kumar <1thenameisshivam@gmail.com>
2025-07-07 16:30:30 +0530

    slide shoe implemented

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,340 +1,30 @@
-/* eslint-disable no-unused-vars */
-import { useQuery } from "@tanstack/react-query";
-import { getUsers, getAnalytics } from "../htttp/api";
-import { Card, List, Avatar, Popover, Statistic, Progress, Table } from "antd";
-import { User, Folder } from "lucide-react";
-import { Pie } from "@ant-design/plots";
-import React from "react";
-
-// Zabbix Static Data
-const zabbixData = [
-  {
-    key: 1,
-    serviceName: "AMISP (MDM, S4-WSE and , Ewatch Read IT)",
-    zabbix: "AMISP-DC and DR zabbix",
-    ip: "172.18.8.75 & 172.19.8.75",
-    dataCentre: "CTrls Mum and Hyd",
-    version: "7.0.10",
-  },
-  {
-    key: 2,
-    serviceName: "Liberty Online2.0",
-    zabbix: "AMISP-DC and DR zabbix",
-    ip: "172.18.8.75 & 172.19.8.75",
-    dataCentre: "CTrls Mum and Hyd",
-    version: "7.0.10",
-  },
-  {
-    key: 3,
-    serviceName: "BBCool",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "6.2.9",
-  },
-  {
-    key: 4,
-    serviceName: "BBCare",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "6.2.9",
-  },
-  {
-    key: 5,
-    serviceName: "Ewatch Online",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ld8",
-    version: "6.2.9",
-  },
-  {
-    key: 6,
-    serviceName: "Ewatch Heat",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ld8 & Ld9",
-    version: "6.2.9",
-  },
-  {
-    key: 7,
-    serviceName: "Sugamlite",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ld8 & Ld9",
-    version: "6.2.9",
-  },
-  {
-    key: 8,
-    serviceName: "Beanbag Heat",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ld8 & Ld9",
-    version: "6.2.9",
-  },
-  {
-    key: 9,
-    serviceName: "Heat Billing-Heat Esco",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ld8 & Ld9",
-    version: "6.2.9",
-  },
-  {
-    key: 10,
-    serviceName: "Prod DAST",
-    zabbix: "M&C Zabbix",
-    ip: "172.16.239.12",
-    dataCentre: "Ld8 & Ld9",
-    version: "6.2.9",
-  },
-  {
-    key: 11,
-    serviceName: "Ewatch read IT-MBC",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 12,
-    serviceName: "Energy Audit- MBC",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 13,
-    serviceName: "EMBC",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 14,
-    serviceName: "GMBC",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 15,
-    serviceName: "MBC Insight",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.6",
-  },
-  {
-    key: 16,
-    serviceName: "MSEDCL",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 17,
-    serviceName: "NPCL",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 18,
-    serviceName: "Adaptricity",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 19,
-    serviceName: "JBVNL",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 20,
-    serviceName: "LVMV",
-    zabbix: "MBC DC zabbix",
-    ip: "10.10.119.76",
-    dataCentre: "Ctrls Noida and Mumbai",
-    version: "7.0.6",
-  },
-  {
-    key: 21,
-    serviceName: "L1-AIS",
-    zabbix: "Liberty zabbix",
-    ip: "192.168.56.43",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.3",
-  },
-  {
-    key: 22,
-    serviceName: "L2-ROW",
-    zabbix: "Liberty zabbix",
-    ip: "192.168.56.43",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.3",
-  },
-  {
-    key: 23,
-    serviceName: "L3-EUM",
-    zabbix: "Liberty zabbix",
-    ip: "192.168.56.43",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.3",
-  },
-  {
-    key: 24,
-    serviceName: "L4-Oman",
-    zabbix: "Liberty zabbix",
-    ip: "192.168.56.43",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.3",
-  },
-  {
-    key: 25,
-    serviceName: "Muti Fuel",
-    zabbix: "Liberty zabbix",
-    ip: "192.168.56.43",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.3",
-  },
-  {
-    key: 26,
-    serviceName: "Sahaj Liberty 150",
-    zabbix: "Liberty zabbix",
-    ip: "192.168.56.43",
-    dataCentre: "Ld8 & Ld9",
-    version: "7.0.3",
-  },
-  {
-    key: 27,
-    serviceName: "Australia P2P-Smart Metering (S2-WSE, Bravo and SFE)",
-    zabbix: "Australia zabbix",
-    ip: "172.16.192.203",
-    dataCentre: "Melbourne and Sydney",
-    version: "7.0.6",
-  },
-  {
-    key: 28,
-    serviceName: "Bravo MDM Services",
-    zabbix: "Australia zabbix",
-    ip: "172.16.192.203",
-    dataCentre: "Melbourne and Sydney",
-    version: "7.0.6",
-  },
-  {
-    key: 29,
-    serviceName: "Republic of Ireland-S3WSE",
-    zabbix: "S3 zabbix",
-    ip: "172.16.201.80",
-    dataCentre: "Ld8",
-    version: "6.2.9",
-  },
-  {
-    key: 30,
-    serviceName: "GBSM (S1-WSE and SFE)",
-    zabbix: "SFE zabbix",
-    ip: "172.16.150.107",
-    dataCentre: "Ld8",
-    version: "6.0.35",
-  },
-  {
-    key: 31,
-    serviceName: "Monitor IT",
-    zabbix: "MonitorIT zabbix",
-    ip: "172.22.231.130",
-    dataCentre: "",
-    version: "6.2.9",
-  },
-  {
-    key: 32,
-    serviceName: "DCC-SMETS1",
-    zabbix: "",
-    ip: "",
-    dataCentre: "",
-    version: "",
-  },
-  {
-    key: 33,
-    serviceName: "Service ERP",
-    zabbix: "",
-    ip: "",
-    dataCentre: "",
-    version: "",
-  },
-  {
-    key: 34,
-    serviceName: "JVVNL",
-    zabbix: "",
-    ip: "",
-    dataCentre: "",
-    version: "",
-  },
-];
-
+import { Card, Table } from "antd";
+import { useEffect, useState } from "react";
+import { zabbixData } from "../utility/constant";
 
 const columns = [
-  {
-    title: "Sr. No.",
-    dataIndex: "key",
-    key: "key",
-    width: 80,
-  },
-  {
-    title: "Service Name",
-    dataIndex: "serviceName",
-    key: "serviceName",
-  },
-  {
-    title: "Zabbix",
-    dataIndex: "zabbix",
-    key: "zabbix",
-  },
-  {
-    title: "Zabbix IP Address",
-    dataIndex: "ip",
-    key: "ip",
-  },
-  {
-    title: "Data Centre",
-    dataIndex: "dataCentre",
-    key: "dataCentre",
-  },
-  {
-    title: "Zabbix Version",
-    dataIndex: "version",
-    key: "version",
-  },
+  { title: "Sr. No.", dataIndex: "key", key: "key", width: 80 },
+  { title: "Service Name", dataIndex: "serviceName", key: "serviceName" },
+  { title: "Zabbix", dataIndex: "zabbix", key: "zabbix" },
+  { title: "Zabbix IP Address", dataIndex: "ip", key: "ip" },
+  { title: "Data Centre", dataIndex: "dataCentre", key: "dataCentre" },
+  { title: "Zabbix Version", dataIndex: "version", key: "version" },
 ];
 
+const PAGE_SIZE = 10;
+
 function Dashboard() {
-  // Fetch Users and Analytics Data
-  const { data } = useQuery({
-    queryKey: ["users"],
-    queryFn: getUsers,
-    placeholderData: (prevData) => prevData,
-  });
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const { data: analyticdata } = useQuery({
-    queryKey: ["analytics"],
-    queryFn: getAnalytics,
-    placeholderData: (prevData) => prevData,
-  });
+  // Autoplay pagination logic
+  useEffect(() => {
+    const totalPages = Math.ceil(zabbixData.length / PAGE_SIZE);
+    const interval = setInterval(() => {
+      setCurrentPage((prev) => (prev < totalPages ? prev + 1 : 1));
+    }, 5000); // 5 seconds
 
-  const rolePriority = { manager: 1, engineer: 2, analyst: 3 };
-  const sortedUsers = data?.data
-    ? [...data.data].sort((a, b) => rolePriority[a.role] - rolePriority[b.role])
-    : [];
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -342,12 +32,15 @@ function Dashboard() {
         Global Network Operation Centre
       </h1>
 
-      {/* Zabbix Monitoring Table */}
       <Card title="Zabbix Monitoring Services" className="mb-8">
         <Table
           columns={columns}
           dataSource={zabbixData}
-          pagination={{ pageSize: 10 }}
+          pagination={{
+            pageSize: PAGE_SIZE,
+            current: currentPage,
+            onChange: (page) => setCurrentPage(page),
+          }}
           bordered
           scroll={{ x: "max-content" }}
         />
